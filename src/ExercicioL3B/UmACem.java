d243c5030612d45ba9fcab8ba192316fe32cf8a4
will2375 <wil.liam_benicio@hotmail.com>
2022-06-24 23:50:58 -0300

    Exercicios L3B um a cem

---
+++ src/ExercicioL3B/UmACem.java
@@ -0,0 +1,15 @@
+package ExercicioL3B;
+
+public class UmACem {
+    public static void main(String[] args) {
+
+        int numero = 1;
+        while (numero <= 100) {
+            System.out.println("contagem while: " + numero);
+            numero++;
+        }
+        for (int num = 1; num <= 100 ;num++)
+            System.out.println("contagem for: " + num);
+
+    }
+}
